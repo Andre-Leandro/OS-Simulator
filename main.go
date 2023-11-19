fd9e27d22594ebdf10e11a0754174fe3d4419424
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-11-19 16:52:27 -0300

    cleaner code

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func (os *OS) addReady(l *[]Process) {
 			break
 		}
 
-		if copy[index].arrivalTime <= os.time { // menor o igual tambien
+		if copy[index].arrivalTime <= os.time { 
 			os.queue = append(os.queue, copy[index])
 			if len(*l) > 0 {
 				if len(*l) == 1 {
@@ -108,7 +108,7 @@ func bestFit(m *Memory, p *Process, os *OS) {
 	internalFragmentation = math.MaxInt
 
 	for index := range m.partitions {
-		partition := (*m).partitions[index] // Obtener una referencia a la partición real en la estructura Memory
+		partition := (*m).partitions[index] 
 		if partition.state && partition.size >= p.size {
 			empty := partition.size - p.size
 			if empty < internalFragmentation {
@@ -116,37 +116,20 @@ func bestFit(m *Memory, p *Process, os *OS) {
 			}
 		}
 	}
-	//fmt.Println("primaria", os.queue)
 
 	if idPartition == -1 {
 		idPartition = bestFitSwap(*m, *p, *os)
 		swapOut(idPartition + 1)
-
-		//(*os).memory.partitions[idPartition].process.loaded = false\
-
 		currentProcess := os.memory.partitions[idPartition].process
-		// currentProcess.loaded = false
-		// currentProcess.time = currentProcess.time - quantum
-
-		// Encontrar el índice del proceso en os.queue con el mismo ID
 		for i, queueProcess := range os.queue {
 			if queueProcess.pid == currentProcess.pid {
 				currentProcess = os.queue[i]
 				currentProcess.loaded = false
-				// Reemplazar el proceso en os.queue con el nuevo proceso de la partición
 				os.queue[i] = currentProcess
 				break
 			}
 		}
-
-		//fmt.Println((*os).memory.partitions[idPartition].process)
-		//fmt.Println("gargante", os.queue)
 	}
-	// selectedPartition := &m.partitions[idPartition]
-	// selectedPartition.state = false // Ocupado
-	// selectedPartition.internalFragmentation = selectedPartition.size - p.size
-	// selectedPartition.process = *p
-	// selectedPartition.process.loaded = newLoaded
 	(*m).partitions[idPartition].state = false // Ocupado
 	(*m).partitions[idPartition].internalFragmentation = (*m).partitions[idPartition].size - (*p).size
 	(*p).loaded = true
@@ -199,10 +182,6 @@ func (p *Process) timeOut(quantum int, queue *[]Process, os *OS, cola *[]Process
 	}
 }
 
-func freeMemory() {
-
-}
-
 func (p Process) isEmpty() bool {
 	return p.pid == 0 && p.time == 0 && p.arrivalTime == 0 && p.size == 0 // and loaded
 }
@@ -217,13 +196,9 @@ func ReadProcessesFromFile(filename string) ([]Process, error) {
 		return nil, err
 	}
 	defer file.Close()
-
 	processes := []Process{}
-
 	scanner := bufio.NewScanner(file)
-	// Agregar una verificación para ignorar la primera línea
 	if scanner.Scan() {
-		// Ignorar la primera línea (encabezado o comentario)
 	}
 
 	for scanner.Scan() {
@@ -250,7 +225,6 @@ func ReadProcessesFromFile(filename string) ([]Process, error) {
 		}
 
 		turnaroundTime := -1
-
 		process := Process{pid, size, arrivalTime, turnaroundTime, time, false}
 		processes = append(processes, process)
 	}
@@ -258,23 +232,8 @@ func ReadProcessesFromFile(filename string) ([]Process, error) {
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
-
 	return processes, nil
 }
-
-/* func printPartitionInfo(memoria Memory) {
-	partition := memoria.partitions[0]
-	state := "Occupied"
-	if partition.state {
-		state = "Free"
-	}
-	processName := "N/A"
-	if partition.process.pid != 0 {
-		processName = fmt.Sprintf("Process-%d", partition.process.pid)
-	}
-	fmt.Printf("| %-10d | %-10d | %-10s | %-15d | %-10s |\n",
-		partition.id, partition.size, state, partition.internalFragmentation, processName)
-} */
 
 func quicksort2(processes []Process) []Process {
 	if len(processes) <= 1 {
@@ -304,43 +263,6 @@ func quicksort2(processes []Process) []Process {
 	}
 	return append(append(quicksort2(less), equal...), quicksort2(greater)...)
 }
-
-/* func printStatistics(completedProcesses []Process, allProcesses []Process) {
-
-	fmt.Println("+" + strings.Repeat("-", 46) + "+")
-	fmt.Printf("| %-4s | %-18s | %-16s |\n", "PID", "Tiempo de Retorno", "Tiempo de Espera")
-	fmt.Println("+" + strings.Repeat("-", 46) + "+")
-	var totalTurnaroundTime, totalWaitTime int
-
-	for _, p := range completedProcesses {
-		// Buscar el proceso correspondiente en allProcesses
-		var originalProcess Process
-		for _, originalP := range allProcesses {
-			if originalP.pid == p.pid {
-				originalProcess = originalP
-				break
-			}
-		}
-
-		waitTime := p.turnaroundTime - originalProcess.time //Poco Optimo Atte André
-		fmt.Printf("| %-4d | %-18d | %-16d |\n", p.pid, p.turnaroundTime, waitTime)
-
-		// Sumar tiempos para el cálculo promedio
-		totalTurnaroundTime += p.turnaroundTime
-		totalWaitTime += waitTime
-	}
-	fmt.Println("+" + strings.Repeat("-", 46) + "+")
-
-	// Calcular promedios
-	averageTurnaroundTime := float64(totalTurnaroundTime) / float64(len(completedProcesses))
-	averageWaitTime := float64(totalWaitTime) / float64(len(completedProcesses))
-
-	// Imprimir tiempos promedio en un mini cuadro
-	fmt.Println("+" + strings.Repeat("-", 46) + "+")
-	fmt.Printf("| %-18s | %-23.2f |\n", "Promedio Retorno", averageTurnaroundTime)
-	fmt.Printf("| %-18s | %-23.2f |\n", "Promedio Espera", averageWaitTime)
-	fmt.Println("+" + strings.Repeat("-", 46) + "+")
-} */
 
 func filterProcessesBySize(processes []Process, sizeThreshold int) ([]Process, []Process) {
 	var filteredProcesses []Process
@@ -380,7 +302,6 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
-
 	var cola []Process
 
 	/* var del []Process */
@@ -395,11 +316,8 @@ func main() {
 	(&linux).initialize(memoria)
 	cola = append(cola, processes...)
 	cola = quicksort2(cola)
-	//fmt.Println(cola)
-
 	cola, _ = filterProcessesBySize(cola, biggestPartition)
 	/* fmt.Println(del) */
-
 	var input string
 	fmt.Print("Inicio del Sistema Operativo - Presione ENTER para continuar")
 
@@ -408,8 +326,6 @@ func main() {
 		if input == "" {
 			if !linux.processor.process.isEmpty() {
 				linux.processor.process.timeOut(quantum, &linux.queue, &linux, &cola)
-
-				//to not go out of bounds
 				if len(linux.queue) == 0 && len(cola) > 0 && linux.processor.process.time <= 0 { //ver por que no funciona con el igual
 					linux.time = cola[0].arrivalTime
 					linux.addReady(&cola)
@@ -430,8 +346,6 @@ func main() {
 				linux.time = cola[0].arrivalTime
 				linux.addReady(&cola)
 				bestFit(&linux.memory, &linux.queue[0], &linux)
-				//fmt.Print("leyenda")
-				//fmt.Print(*&linux.memory.partitions[2])
 				linux.processor.process = linux.queue[0]
 				linux.queue = append(linux.queue[1:])
 			}
@@ -463,8 +377,6 @@ func main() {
 			break
 		}
 	}
-
 	fmt.Println("\n", "CUADRO ESTADÍSTICO")
 	arrancar(linux.completedProcesses, processes)
-
 }
